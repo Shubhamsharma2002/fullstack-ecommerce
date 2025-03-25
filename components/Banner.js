d6cb518697cccea48f5e2a40c75d6aa301d716f3
shubham sharma <shubhamtech004@gmail.com>
2025-03-25 18:19:23 +0530

    using/setting of material ui icon for cloures

--- components/Banner.js
+++ components/Banner.js
@@ -1,23 +1,48 @@
 import React, { useEffect, useState } from "react";
 import Styles from "../styles/Banner.module.css"
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 const Banner = () =>{
     const [product,setproduct] = useState([])
     const [selectedproduct, setSeletecdproduct] = useState(null)
+    const [index,setindex] = useState(0);
     const getAllproduct =()=>{
         fetch('https://dummyjson.com/products')
         .then((res) => res.json())
         .then((res) => {
-            setproduct(res.products);
+            setproduct(res.products.slice(0,5));
             setSeletecdproduct(res.products[0]);
         })
         .catch(error => console.log(error)
         )
     }
+    // console.log(product);
+    
 
    useEffect(() =>{
     getAllproduct()
    },[])
-//    console.log(product);
+  
+   const handleNextArrowclick = () =>{
+    if(index == product.length-1){
+        setSeletecdproduct(product[0])
+        setindex(0)
+        return;
+    }
+     setSeletecdproduct(product[index+1])
+     setindex(index+1)
+   }
+   const handlePreviousArrowclick = () =>{
+    if(index == 0){
+        setSeletecdproduct(product[product.length-1])
+        setindex[index-1]
+        return
+    }
+    setSeletecdproduct(product[index-1])
+    setindex(index-1)
+    
+    
+   }
    
     return(
         // <div>
@@ -31,8 +56,10 @@ const Banner = () =>{
         //    ))}
         // </div>
 
-        <div>
+        <div className={Styles.banner}>
+            <ArrowBackIosIcon onClick={handlePreviousArrowclick} />
         <img className={Styles.banner_img} src={selectedproduct?.thumbnail}  />
+        <ArrowForwardIosIcon onClick={handleNextArrowclick} />
         </div>
         
 
