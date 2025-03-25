a81436fd1ed36b5162646aa2f2b2d22ac3ae4410
shubham sharma <shubhamtech004@gmail.com>
2025-03-25 17:42:37 +0530

    banner component done with the css

--- components/Banner.js
+++ components/Banner.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
-
+import Styles from "../styles/Banner.module.css"
 const Banner = () =>{
     const [product,setproduct] = useState([])
-
+    const [selectedproduct, setSeletecdproduct] = useState(null)
     const getAllproduct =()=>{
         fetch('https://dummyjson.com/products')
         .then((res) => res.json())
-        .then((res)=>setproduct(res.products))
+        .then((res) => {
+            setproduct(res.products);
+            setSeletecdproduct(res.products[0]);
+        })
         .catch(error => console.log(error)
         )
     }
@@ -17,15 +20,19 @@ const Banner = () =>{
 //    console.log(product);
    
     return(
+        // <div>
+        //    {product.map((p)=>(
+        //     <>
+        //     <img src={p.thumbnail} alt={p.name} />
+        //     <h3>{p.title}</h3>
+        //     <p>{p.name}</p>
+        //     <p>ID: {p.id}</p>
+        //   </>
+        //    ))}
+        // </div>
+
         <div>
-           {product.map((p)=>(
-            <>
-            <img src={p.thumbnail} alt={p.name} />
-            <h3>{p.title}</h3>
-            <p>{p.name}</p>
-            <p>ID: {p.id}</p>
-          </>
-           ))}
+        <img className={Styles.banner_img} src={selectedproduct?.thumbnail}  />
         </div>
         
 
