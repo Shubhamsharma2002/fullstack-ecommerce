3ac1f9bcf9eeeca5699e970050423ab51bb211ac
shubham sharma <shubhamtech004@gmail.com>
2025-03-26 19:26:14 +0530

    comlete with the card section top product section with the api calling data displaying

--- components/Topproduct.js
+++ components/Topproduct.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
-
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import Styles from "../styles/ProductCard.module.css"
 const Topproducts = ()=>{
     const [products,setproduct] = useState([])
     const getAllproduct =()=>{
         fetch('https://dummyjson.com/products')
         .then((res) => res.json())
         .then((res)=>{
-             setproduct(res.products[0])
+             setproduct(res.products.slice(0,8))
         })
         .catch(error => console.log(error)
         )
@@ -20,9 +21,27 @@ const Topproducts = ()=>{
  },[])
    return(
       <div>
-        <p>Top Products</p>
-        <ProductCard id={products.id} title={products.title} brand={products.brand} price={products.price} image={products.images} rating={products.rating}/>
+         <p className={Styles.top_product_title}>Top Products</p>
+      
+      <div className={Styles.top_product_card}>
+        
+        {products?.map((p)=>(
+            <ProductCard 
+            id={p.id} 
+            title={p.title} 
+            brand={p.brand} 
+            price={p.price} 
+            image={p.images} 
+            rating={p.rating}
+            />
+        ))}
+        
        
+      </div>
+      <div className={Styles.veiw_all}>
+         <p>View All</p>
+         <ArrowForwardIcon/>
+      </div>
       </div>
    )
 }
