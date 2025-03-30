e13e9a3be99db51315e73e57f6d90c67a1cd2a76
shubham sharma <shubhamtech004@gmail.com>
2025-03-30 18:05:39 +0530

    footer link clicable

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,17 @@
 import React from "react";
 import Styles from "../styles/Footer.module.css"
+import Link from "next/link";
 const Footer = () =>{
     return(
         <div className={Styles.footer}>
             <div>
-                <p>Logo</p>
+                <Link href="/homepage">Logo</Link>
+                
             </div>
             <div className={Styles.footer_links}>
-              <p>My acount</p>
-              <p>Cart</p>
-              <p>Contact</p>
+              <Link className={Styles.footer_links_link} href="/myaccount">My acount</Link>
+              <Link className={Styles.footer_links_link} href="/cart">Cart</Link>
+              <Link className={Styles.footer_links_link} href="/contact">Contact</Link>
             </div>
         </div>
     )
