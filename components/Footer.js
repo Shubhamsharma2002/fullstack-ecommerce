897e118d92e527d3c4874f5d714d5c871e9b200e
shubham sharma <shubhamtech004@gmail.com>
2025-03-30 17:57:30 +0530

    foter section done

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,15 @@
 import React from "react";
-
+import Styles from "../styles/Footer.module.css"
 const Footer = () =>{
     return(
-        <div>
+        <div className={Styles.footer}>
             <div>
                 <p>Logo</p>
             </div>
-            <div>
-                ALL rihht resevred
+            <div className={Styles.footer_links}>
+              <p>My acount</p>
+              <p>Cart</p>
+              <p>Contact</p>
             </div>
         </div>
     )
