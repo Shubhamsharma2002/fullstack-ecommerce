36383d03933e1af7be80757403714d492945721d
shubham sharma <shubhamtech004@gmail.com>
2025-03-31 00:29:21 +0530

    product details page done

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
-
+import Styles from "../../styles/ProductDetails.module.css"
+import StarIcon from '@mui/icons-material/Star';
 const Product = () =>{
     const router = useRouter()
     console.log(router.query.id);
@@ -20,22 +21,34 @@ const Product = () =>{
         }
     },[router.query.id])
     console.log("by usestate",product);
+    // {product?.images?.map((img, id) => {
+    //     console.log(img);  
+    //     return id;
+    // })}
     
     return(
-        <div>
-            <div>
-               <p>{product.title}</p>
-               <p>Category:{product.category}</p>
-               <p>{product.description}</p>
+        <div className={Styles.product_details}>
+            <div className={Styles.product_details_data}>
+               <p className={Styles.product_details_data_title}>{product.title}</p>
+               <p className={Styles.product_details_data_brand}>Brand:{product.brand}</p>
+               <p className={Styles.product_details_data_category}>Category:{product.category}</p>
+               <p className={Styles.product_details_data_discription}>{product.description}</p>
+            <div className={Styles.product_details_data_pricing}>
+                <p className={Styles.product_details_data_price}>₹{product.price}</p>
+                <p className={Styles.product_details_data_discount}>Discount % : {product.discountPercentage}</p>
             </div>
-            <div>
-                <p>₹{product.price}</p>
-                <p>Discount % : {product.discountPercentage}</p>
+             <div className={Styles.product_details_data_last}>
+             <p className={Styles.product_details_data_last_iteleft}>Item Left : {product.stock}</p>
+             <p className={Styles.product_details_data_last_rating}><StarIcon/>{product.rating}</p>
+             </div>
+             <button className={Styles.add_to_cart}>Add to Cart</button>
             </div>
-            <p>{product.rating}</p>
-            <p>Item Left : {product.stock}</p>
-            <div>
-                <img src={product.thumbnail} width="100px"/>
+            <div className={Styles.images}>
+                
+                <img src={product.thumbnail} />
+                {/* {product?.images?.map((img,id)=>(
+                     <img src={img} alt={`Product image ${id}`}/>
+                ))} */}
             </div>
         </div>
     )
