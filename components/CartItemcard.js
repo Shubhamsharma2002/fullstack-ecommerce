3a273d6ca2671c71943b816bb740c6e3ac8e912a
shubham sharma <shubhamtech004@gmail.com>
2025-04-02 18:18:44 +0530

    carditem card component done

---
+++ components/CartItemcard.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const CartItemcard = ({id,title,description,image,category,brand,discountPercentage,price,rating}) =>{
+
+    return(
+        <div>
+            <div>
+                <p>{title}</p>
+                <p>{brand}</p>
+                <p>Category:{category}</p>
+                <p>{description}</p>
+                <div>
+                    <p>{price}</p>
+                    <p>{discountPercentage}</p>
+                    <p>{rating}</p>
+                </div>
+            </div>
+            <div>
+                <img src={image}/>
+            </div>
+        </div>
+    )
+}
